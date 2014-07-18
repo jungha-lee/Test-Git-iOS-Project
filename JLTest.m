bfb7a8ded91b88423e8b219bcf943a42b88caeea
Jungha Lee <junghalee@Jungha-Lees-MacBook-Pro.local>
2014-07-18 20:40:39 +0200

    added a test class

---
+++ JLTest.m
@@ -0,0 +1,13 @@
+//
+//  JLTest.m
+//  C10-Git test
+//
+//  Created by Jungha Lee on 7/18/14.
+//  Copyright (c) 2014 Jungha Lee. All rights reserved.
+//
+
+#import "JLTest.h"
+
+@implementation JLTest
+
+@end
